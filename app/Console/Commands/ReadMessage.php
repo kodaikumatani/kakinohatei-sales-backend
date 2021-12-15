56efde67b97b847a0b380d8b4e7b39201ec3d446
kodaikumatani <koudai.aco5423@gmail.com>
2021-12-15 23:44:49 +0900

    Add: Entry new command (gmail:send, gmail:read)

---
+++ app/Console/Commands/ReadMessage.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Service\ManageMailboxes;
+
+class ReadMessage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gmail:read';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Read a message in UNREAD box';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = new ManageMailboxes();
+        $str = $client->getMessage();
+        print_r($str);
+    }
+}
---
+++ app/Console/Commands/SendBlankEmail.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Service\SendEmail;
+
+class SendBlankEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gmail:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send a blank e-mail to JAinaba.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = new SendEmail();
+        $client->toJAinaba();
+    }
+}
