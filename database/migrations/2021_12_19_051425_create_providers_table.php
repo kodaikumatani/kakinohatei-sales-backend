6ab1a7a702ace7c58b93397c5bc0487a0846bcae
kodaikumatani <koudai.aco5423@gmail.com>
2021-12-19 14:47:39 +0900

    Clean：Change table names to plural

--- database/migrations/2021_12_19_051425_create_providers_table.php
+++ database/migrations/2021_12_19_051425_create_providers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProviderTable extends Migration
+class CreateProvidersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProviderTable extends Migration
      */
     public function up()
     {
-        Schema::create('provider', function (Blueprint $table) {
+        Schema::create('providers', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedSmallInteger('provider_id');
             $table->string('name', 100);
@@ -29,6 +29,6 @@ class CreateProviderTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('provider');
+        Schema::dropIfExists('providers');
     }
 }
--- database/migrations/2021_12_19_051449_create_stores_table.php
+++ database/migrations/2021_12_19_051449_create_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStoreTable extends Migration
+class CreateStoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateStoreTable extends Migration
      */
     public function up()
     {
-        Schema::create('store', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedSmallInteger('store_id');
             $table->string('name', 100);
@@ -29,6 +29,6 @@ class CreateStoreTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('store');
+        Schema::dropIfExists('stores');
     }
 }
