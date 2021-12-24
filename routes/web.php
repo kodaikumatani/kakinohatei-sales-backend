04ea3db11c22ca367e932134b55c38a82f51da27
kodaikumatani <koudai.aco5423@gmail.com>
2021-12-24 12:47:30 +0000

    Add：Deploying React Material UI

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+*/
+Route::get('/{any}', function(){
+    return view('app');
+})->where('any', '.*');
