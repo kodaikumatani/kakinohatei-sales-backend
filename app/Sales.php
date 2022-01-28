95885a653e555916684dd2e9c103df14369f9261
kodaikumatani <koudai.aco5423@gmail.com>
2022-01-28 09:50:11 +0000

    Update:Calculate monthly sales from accoutings table

--- app/Sales.php
+++ app/Sales.php
@@ -2,11 +2,12 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class Sales extends Model
 {
-	private function timeOfLastReceive()
+	public function timeOfLastReceive()
 	{
 		return $this
 			->select('received_at')
@@ -17,45 +18,37 @@ class Sales extends Model
 			->received_at;
 	}
 	
-	public function summary()
+	public function daily()
 	{
-		$received_at = $this->timeOfLastReceive();
-		$daily = $this
+		return $this
 			->selectRaw('sum(price * quantity) as summary')
-			->where('received_at', $received_at)
+			->where('received_at', $this->timeOfLastReceive())
 			->get()[0]
 			->summary;
-		
-		$this_month_mails = $this
-			->selectRaw("max(received_at) as date")
-			->groupByRaw("DATE_FORMAT(received_at, '%Y%m%d')")
-			->where('received_at', 'like', substr($received_at,0,7).'%')
-			->get();
-		
-		$monthly = 0;
-		foreach($this_month_mails as $mail) {
-			$monthly += $this
-				->selectRaw('sum(price * quantity) as sales')
-				->where('received_at', $mail->date)
-				->get()[0]
-				->sales;
-		}
-		return array('received'=>$received_at, 'daily'=>$daily, 'monthly'=>$monthly);
+	}
+	
+	public function summary()
+	{
+		$received_at =  $this->timeOfLastReceive();
+		$monthly = DB::table('accountings')
+			->where('received_at', 'like', substr($received_at,0,7)."%")
+			->sum('sales');
+		return array('received'=>$received_at, 'daily'=>$this->daily(), 'monthly'=>$monthly);
 	}
 	
 	public function chart()
 	{
 		$data = [];
-		$lastReceive = substr($this->timeOfLastReceive(),0,11);
-		
-		for ($hour=10; $hour<=19; $hour++) {
-			$query = $lastReceive.$hour.'%';
+		$lastReceivedDate = substr($this->timeOfLastReceive(),0,11);
+		for ($hour=10; $hour<=21; $hour++) {
+			$query = $lastReceivedDate.$hour.'%';
+			
 			$lastTimeInHour = $this
 				->selectRaw('max(received_at) as lastdate')
 				->where('received_at', 'like', $query)
 				->get()[0]
 				->lastdate;
-			
+
 			$salesHour = $this
 				->selectRaw('sum(price * quantity) as amount')
 				->where('received_at', $lastTimeInHour)
