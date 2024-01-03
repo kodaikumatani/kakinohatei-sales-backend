7c90751a977712b411d3c569b02bdf206f45d0f6
kodaikumatani <koudai.aco5423@gmail.com>
2024-01-03 22:33:44 +0900

    feat: 売上の小計を保存する

--- database/migrations/2022_10_06_060249_create_sales_table.php
+++ database/migrations/2022_10_06_060249_create_sales_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->dateTime('date');
+            $table->date('date');
             $table->unsignedInteger('hour');
             $table->foreignId('user_id')
                 ->constrained('users')
